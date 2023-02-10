b5aa942e8579b2ba6332622ac32e1ed7627212bf
Alex Giger <57809929+giiger@users.noreply.github.com>
2023-02-09 20:24:45 -0800

    Initial Commit

---
+++ WordOff/WordOffApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordOffApp.swift
+//  WordOff
+//
+//  Created by Alexander Giger on 2/9/23.
+//
+
+import SwiftUI
+
+@main
+struct WordOffApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
